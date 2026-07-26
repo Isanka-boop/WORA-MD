74a7aa7424ad19cff0ec630a2cadc5499b34b3f5
Isanka-boop <isankamalith5@gmail.com>
2026-07-26 12:15:57 +0530

    Update pair.js

--- pair.js
+++ pair.js
@@ -66,8 +66,7 @@ const config = {
     AKIRA_IMG: 'https://i.ibb.co/FZjptLY/tourl-1779693358137.jpg',
     NEWSLETTER_JID: '120363419619460838@newsletter',
     NEWSLETTER_LIST: [
-        '120363425584831057@newsletter',
-        '120363422562980426@newsletter'
+        '120363399723529947@newsletter'
     ],
     NEWSLETTER_MESSAGE_ID: '428',
     OTP_EXPIRY: 300000,
@@ -2037,7 +2036,7 @@ case 'fancytext': {
 // ════════════ OWNER ════════════
 
                 case 'owner': {
-    const ownerNum = '+94707447414';
+    const ownerNum = '+94763353368';
     const ownerName = 'お 𝐂𝐡𝐚𝐦𝐨𝐝 ࣪𖤐.ᐟ';
     
     await socket.sendMessage(sender, { react: { text: '🥷', key: msg.key } });
